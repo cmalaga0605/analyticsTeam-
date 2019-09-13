c43cfd175f79bf014f409c6dc52083fd1dad42af
Cristian Malaga <47251490+cmalaga0605@users.noreply.github.com>
2019-09-12 21:16:56 -0400

    Add files via upload
    *Difficulty 1.3

---
+++ R2 - Kattis Problem.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[23]:
+
+
+R1 , s = list(map(int, input().split()))  
+if (R1 >= -1000 and R1 <=1000)and (s >= -1000 and R1 <=1000):
+    R2 = 2*s - R1
+    print(R2)
+    
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
